dc3a2dd393c3504892634444cde87ff8c2432594
Rajan <76466434+neupane-rajan@users.noreply.github.com>
2026-04-12 14:09:44 +0000

    test update

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-# this is after i will pull my code
+math = {"a","b","c","d","z","y"}
+science = {"a","b","e","f","g"}
+
+# common students
+
+s_and_m = math & science
+
+# aauta matra subject padne student
+only_one_subject = math^science
+print(only_one_subject)
+# only science
+o_s = science-math
+print(o_s)
+# only math
+o_m = math-science
+print(o_m)
+
+
+# all students
+
+all_students = math | science
+print(all_students)
