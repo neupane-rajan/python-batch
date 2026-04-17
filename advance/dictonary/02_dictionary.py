a1b8087bacefcfc3da2f43b7aa4db4e782a2b44b
Rajan Neupane <rajanneupane355@gmail.com>
2026-04-17 20:22:23 +0545

    02_dictionary.py

---
+++ advance/dictonary/02_dictionary.py
@@ -0,0 +1,18 @@
+# looping
+# extracting keys from dictionary
+students = {"1": "Dhirendra", "2": "shirish", "3": "Bharat"}
+# for key in students:
+#     print(key)
+
+# # extracting values from dictionary
+
+# for value in students.values():
+#     print(value)
+
+# extracting key and values from dictonary using loop
+
+for key,value in students.items():
+    print(key,value)
+
+
+# research about .key(), .values(),.items(),.update()
