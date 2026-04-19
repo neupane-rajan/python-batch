c6635c9192bc5ac324a04bad9fd3e612ebeaad25
Rajan Neupane <rajanneupane355@gmail.com>
2026-04-19 19:53:24 +0545

    02_dictionary.py completed

--- advance/dictonary/02_dictionary.py
+++ advance/dictonary/02_dictionary.py
@@ -1,6 +1,6 @@
 # looping
 # extracting keys from dictionary
-students = {"1": "Dhirendra", "2": "shirish", "3": "Bharat"}
+# students = {"1": "Dhirendra", "2": "shirish", "3": "Bharat", "4": "dipak"}
 # for key in students:
 #     print(key)
 
@@ -11,8 +11,27 @@ students = {"1": "Dhirendra", "2": "shirish", "3": "Bharat"}
 
 # extracting key and values from dictonary using loop
 
-for key,value in students.items():
-    print(key,value)
+# for key,value in students.items():
+#     print(key,value)
 
 
-# research about .key(), .values(),.items(),.update()
+# Important Built-in method
+
+# keys() : yesle dictionary bhitra vako keys return garxa
+# print(students.keys())
+
+# values(): yesle dictionary bhitra vako values haru  return garxa
+# print(students.values())
+
+
+# items() combination of keys and values in dictionary is called items
+
+# print(type(students.items()))
+
+# update() : update our existing dictionary and if we try to print we got None as return value
+# print(students.update({"city": "lamki"}))
+# print(students)
+
+
+
+
